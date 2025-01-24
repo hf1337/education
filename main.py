73d8565c69386c129ea30203ac8230c6f2334024
hf1337 <hf1337@bk.ru>
2025-01-24 18:05:53 +0300

    Конкатенация.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 '''Education file.'''
 
-# * Экранированнные последовательности. Перенос на другую строку \n.
-print('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
+# * Конкатенация слов.
+print('Winter ' + 'came' + 'for ' + 'the ' + 'House ' + 'of ' + 'Frey.')
