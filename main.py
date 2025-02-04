eb6f7740d15a9f26174cd6e8bcb4c223eb4cd094
hf1337 <hf1337@bk.ru>
2025-02-04 18:30:49 +0300

    Выражения в определениях.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 """Education file."""
 
-# * Выбор имени переменной.
-my_brothers_count = 1
+euros_count = 100
+
+# Евро в долларах
+dollar_count = 100 * 1.25
+print(dollar_count)
+
+# Доллары в юанях
+yuans_count = dollar_count * 6.91
+print(yuans_count)
+
+print('Euros: ' + str(euros_count) + ' ' + 'Dollars: ' + str(dollar_count) + ' ' + 'Yuans: ' + str(yuans_count) + ' ')
